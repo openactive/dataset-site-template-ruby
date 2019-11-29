7696dac97fd8283e26ac8cb14c023dcb61ea6360
Joe Carter <joe@simpleweb.co.uk>
2019-11-29 15:31:13 +0000

    Move example + simplify

--- examples/example.rb
+++ examples/example.rb
@@ -3,7 +3,14 @@ require 'bundler/setup'
 
 require "openactive/dataset"
 
-settings = {
+feed_types = [
+  OpenActive::Dataset::FeedType::FACILITY_USE,
+  OpenActive::Dataset::FeedType::SCHEDULED_SESSION,
+  OpenActive::Dataset::FeedType::SESSION_SERIES,
+  OpenActive::Dataset::FeedType::SLOT,
+]
+
+settings = OpenActive::Dataset::Settings.new(
   open_data_feed_base_url: "http://example.com/feed/",
   dataset_site_url: "http://example.com/dataset/",
   dataset_discussion_url: "https://github.com/simpleweb/sw-oa-php-test-site",
@@ -18,20 +25,9 @@ settings = {
   organisation_email: "spam@simpleweb.co.uk",
   background_image_url: "https://simpleweb.co.uk/wp-content/uploads/2017/06/IMG_8994-500x500-c-default.jpg",
   date_first_published: "2019-11-05", # remember, remember the fifth of November...
-}
-
-feed_types = [
-  OpenActive::Dataset::FeedType::FACILITY_USE,
-  OpenActive::Dataset::FeedType::SCHEDULED_SESSION,
-  OpenActive::Dataset::FeedType::SESSION_SERIES,
-  OpenActive::Dataset::FeedType::SLOT,
-]
-
-oa_settings = OpenActive::Dataset::Settings.new(
-  **settings,
   data_feed_types: feed_types,
 )
 
-renderer = OpenActive::Dataset::TemplateRenderer.new(oa_settings)
+renderer = OpenActive::Dataset::TemplateRenderer.new(settings)
 
 puts renderer.render
