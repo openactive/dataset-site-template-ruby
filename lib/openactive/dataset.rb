938e2cb4bfd2652645ac0016a2e404d259dd2523
Joe Carter <joe@simpleweb.co.uk>
2019-12-02 10:34:53 +0000

    Remove commented out require to removed file

--- lib/openactive/dataset.rb
+++ lib/openactive/dataset.rb
@@ -1,7 +1,6 @@
 require "openactive"
 require "openactive/dataset/version"
 
-# require "openactive/dataset/feed_configuration"
 require "openactive/dataset/feed_type"
 require "openactive/dataset/meta"
 require "openactive/dataset/settings"
