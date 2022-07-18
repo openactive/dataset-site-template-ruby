2d62f41188b35a28d93255b440b03ba28971fc3e
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2022-07-18 17:41:34 +0100

    Revert test

--- lib/openactive/dataset_site/settings.rb
+++ lib/openactive/dataset_site/settings.rb
@@ -109,7 +109,7 @@ module OpenActive
 
       def access_service
         OpenActive::Models::WebAPI.new(
-          # name: 'Open Booking API',
+          name: 'Open Booking API',
           description: "API that allows for seamless booking experiences to be created for #{data_feed_descriptions.to_sentence.downcase} available from #{organisation_name}",
           documentation: open_booking_api_documentation_url,
           terms_of_service: open_booking_api_terms_service_url,
